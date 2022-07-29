93dd8da33abca174df0253f2d734e3ba6bb9e3d0
mad-max-fury <chrisnonso8@gmail.com>
2022-07-29 23:03:04 +0100

    added our features to landing page

---
+++ components/images/index.js
@@ -0,0 +1,5 @@
+import duck from "./duck 2.svg";
+import spiral from "./spiral.svg";
+import walletImage from "./wallet.svg";
+
+export { duck, spiral, walletImage };
--- components/landingpage/index.js
+++ components/landingpage/index.js
@@ -1,12 +1,27 @@
 import React from "react";
-import { AboutUs, Hero, HowItWorks, SupportedWalletBanner } from "./components";
+import {
+  AboutUs,
+  Hero,
+  HowItWorks,
+  OurFeatures,
+  SupportedWalletBanner,
+} from "./components";
+import Image from "next/image";
+import { spiral } from "../images";
+import styles from "./style.module.css";
 const LandinPage = () => {
   return (
     <div>
       <Hero />
       <SupportedWalletBanner />
       <AboutUs />
-      <HowItWorks />
+      <div className={styles.Wrap}>
+        <div className={styles.ourFeaturesAbsolute}>
+          <Image src={spiral} alt="spiral" />
+        </div>
+        <HowItWorks />
+        <OurFeatures />
+      </div>
     </div>
   );
 };
