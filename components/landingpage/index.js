ebf054eef0d1d1e3381626b26a75785385053424
mad-max-fury <chrisnonso8@gmail.com>
2022-07-29 04:13:06 +0100

    added how it works to landing page

--- components/landingpage/index.js
+++ components/landingpage/index.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { AboutUs, Hero, SupportedWalletBanner } from "./components";
+import { AboutUs, Hero, HowItWorks, SupportedWalletBanner } from "./components";
 const LandinPage = () => {
   return (
     <div>
       <Hero />
       <SupportedWalletBanner />
       <AboutUs />
+      <HowItWorks />
     </div>
   );
 };
