71e86a79c8c2ba5a796b2906d20c5b100fec75cd
mad-max-fury <chrisnonso8@gmail.com>
2022-08-22 21:21:44 +0100

    updated landingpage

--- components/landingpage/components/hero/index.js
+++ components/landingpage/components/hero/index.js
@@ -9,36 +9,39 @@ import { VideoModal } from "../../../modals/videoModal";
 const Hero = () => {
   const [open, setOpen] = useState(false);
   return (
-    <div className={styles.container}>
-      <div className={styles.hero}>
-        <div className={styles.heroText}>
-          <h1>
-            Democratizing
-            <br />
-            fundraising through
-            <br /> blockchain.
-          </h1>
-          <p>
-            Explore unparalleled access to donors and projects and support
-            <br />
-            innovative change that create the future. With secured funds and
-            <br />
-            records and a transparent process. The future can be created.
-          </p>
-          <div className={styles.playBtn}>
-            <button>
-              <span>
-                <BsFillPlayFill size={25} />
-              </span>
-            </button>
-            <p>Watch video</p>
+    <>
+      <VideoModal open={open} onClose={() => setOpen(!open)} />
+      <div className={styles.container}>
+        <div className={styles.hero}>
+          <div className={styles.heroText}>
+            <h1>
+              Democratizing
+              <br />
+              fundraising through
+              <br /> blockchain.
+            </h1>
+            <p>
+              Explore unparalleled access to donors and projects and support
+              <br />
+              innovative change that create the future. With secured funds and
+              <br />
+              records and a transparent process. The future can be created.
+            </p>
+            <div className={styles.playBtn}>
+              <button onClick={() => setOpen(!open)}>
+                <span>
+                  <BsFillPlayFill size={25} />
+                </span>
+              </button>
+              <p>Watch video</p>
+            </div>
+          </div>
+          <div className={styles.heroImage}>
+            <Image src={walletImage} />
           </div>
         </div>
-        <div className={styles.heroImage}>
-          <Image src={walletImage} />
-        </div>
       </div>
-    </div>
+    </>
   );
 };
 
